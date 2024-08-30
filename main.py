9a9a49e286f7983802dc4d89e4e1b830f77a03b1
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-08-30 23:19:30 +0200

    Initialize

---
+++ main.py
@@ -0,0 +1,35 @@
+name="Brezzy"
+age=21
+weight=90
+is_student=True
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(is_student))
+
+age=str(age)
+weight=int(weight)
+print(type(age))
+print(type(weight))
+
+first_name = "Brian"
+last_name = "Zondi"
+full_name = first_name+last_name
+exam = "Haa"*5
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Full Name:", full_name)
+print("String Multiplied 5 times gives this result:", exam)
+
+x=5
+y=9
+z=1
+
+print("The floor division is",x//y//z)
+print("The modulus is",x%y%z)
+print("The square is",x**y**z)
+print("The square root is",x**0.5**y)
+print("The addition is",x+y+z)
+print("The multiplication",x*y+z)
+print("The subtraction is",x-y-z)
+print("The division is",x/y/z)
